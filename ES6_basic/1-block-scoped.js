fe2fe34f55d765006d1076ed522497ffadb20db7
DiggDiggs <christopherdiggs1@gmail.com>
2023-09-06 01:28:18 +0000

    1done

---
+++ ES6_basic/1-block-scoped.js
@@ -0,0 +1,12 @@
+export default function taskBlock(trueOrFalse) {
+    const task = false;
+    const task2 = true;
+  
+if (trueOrFalse) {
+      const Newtask = true;
+      const Newtask2 = false;
+return [Newtask, Newtask2];
+    }
+
+return [task, task2];
+}
