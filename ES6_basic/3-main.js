55dacd760b0d86fe0399789f5311d58d984fc2a8
DiggDiggs <christopherdiggs1@gmail.com>
2023-09-06 16:53:29 +0000

    main file 3

--- ES6_basic/3-main.js
+++ ES6_basic/3-main.js
@@ -1,11 +1,4 @@
-if (expansion1989 === undefined) {
-    expansion1989 = 89;
-}
-
-if (expansion2019 === undefined) {
-    expansion2019 = 19;
-}
-
-if (initialNumber === undefined) {
-    initialNumber = undefined;
-}
+import getSumOfHoods from './3-default-parameter.js';
+console.log(getSumOfHoods(34));
+console.log(getSumOfHoods(34, 3));
+console.log(getSumOfHoods(34, 3, 4));
