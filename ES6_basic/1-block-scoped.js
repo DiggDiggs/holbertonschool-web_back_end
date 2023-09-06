dbbd591940eddd284b22a353906fb0cd645f51c3
DiggDiggs <christopherdiggs1@gmail.com>
2023-09-06 01:38:47 +0000

    linter

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -2,11 +2,11 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
-if (trueOrFalse) {
+  if (trueOrFalse) {
     const newtask = false;
     const newtask2 = true;
-return [newtask, newtask2];
-}
+  return [newtask, newtask2];
+  }
 
-return [task, task2];
+  return [task, task2];
 }
