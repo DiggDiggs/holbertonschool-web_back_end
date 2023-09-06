e05f9a191099ddf288c5cc9faf3884e232cc2fd5
DiggDiggs <christopherdiggs1@gmail.com>
2023-09-06 16:41:17 +0000

    3default

---
+++ ES6_basic/3-main.js
@@ -0,0 +1,11 @@
+if (expansion1989 === undefined) {
+    expansion1989 = 89;
+}
+
+if (expansion2019 === undefined) {
+    expansion2019 = 19;
+}
+
+if (initialNumber === undefined) {
+    initialNumber = undefined;
+}
