435422e5fd37452902be9f0361eddd5feda9e632
DiggDiggs <christopherdiggs1@gmail.com>
2023-09-07 21:48:51 +0000

    update

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -1,5 +1,5 @@
 export default class ClassRoom {
-    constructor(maxStudintsSize) {
-        this.maxStudintsSize = maxStudintsSize; 
-    }
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
 }
