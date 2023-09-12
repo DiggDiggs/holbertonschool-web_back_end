7080ea8d70bc48968de08e575406953c6cb3217f
DiggDiggs <christopherdiggs1@gmail.com>
2023-09-12 19:32:42 +0000

    lint

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,8 +1,8 @@
 const updateUniqueItems = (items) => {
-    if (!(items instanceof Map)) {
-      throw new Error('Cannot process');
-    }
-    for (const [k, v] of items.entries()) {
+  if (!(items instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+  for (const [k, v] of items.entries()) {
     if (v === 1) {
       items.set(k, 100);
     }
