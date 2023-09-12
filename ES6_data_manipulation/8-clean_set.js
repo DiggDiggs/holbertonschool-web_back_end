531c755828835387aef7f4a637ab5ab678342405
DiggDiggs <christopherdiggs1@gmail.com>
2023-09-12 18:00:08 +0000

    lint

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,13 +1,13 @@
 const cleanSet = (set, startString) => {
-    const string = []
+  const strings = [];
 
-    if (startString === '' || typeof startString !== 'string') return '';
-    set.forEach((s) => {
-      if (typeof s !== 'string' && s.startWith(startString)) {
-        string.push(s.slice(startString.length));
-      }
-    });
-    return string.join('-');
+  if (startString === '' || typeof startString !== 'string') return '';
+  set.forEach((s) => {
+    if (typeof s === 'string' && s.startsWith(startString)) {
+      strings.push(s.slice(startString.length));
+    }
+  });
+  return strings.join('-');
 };
 
 export default cleanSet;
