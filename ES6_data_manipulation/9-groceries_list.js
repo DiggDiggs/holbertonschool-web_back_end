adc80520ea036c017dd3e7959fc824c5c1fd40b2
DiggDiggs <christopherdiggs1@gmail.com>
2023-09-12 18:46:05 +0000

    lint

--- ES6_data_manipulation/9-groceries_list.js
+++ ES6_data_manipulation/9-groceries_list.js
@@ -2,9 +2,9 @@ const groceriesList = () => {
   const map = new Map();
   map.set('Apples', 10);
   map.set('Tomatoes', 10);
-  map.set ('pasta', 1);
-  map.set ('Rice', 1);
-  map.set ('Banana', 5);
+  map.set('pasta', 1);
+  map.set('Rice', 1);
+  map.set('Banana', 5);
   return map;
 };
 
